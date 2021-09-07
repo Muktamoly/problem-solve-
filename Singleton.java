1032a69e18ce4cf6223829698cf638b75bca173b
Mukta Akanda Moly <90136622+Muktamoly@users.noreply.github.com>
2021-09-07 22:02:17 +0600

    initial commit

---
+++ Singleton.java
@@ -0,0 +1,22 @@
+
+class Singleton {
+
+    private static Singleton single_instance = null;
+
+
+    public String v;
+
+    private Singleton()
+    {
+        v = "Hello ";
+    }
+
+
+    public static Singleton getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new Singleton();
+
+        return single_instance;
+    }
+}
